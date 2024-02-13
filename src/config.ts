86d9da51be7644fad712c91eb0a53b575c915961
luidimso <luidi_mso@outlook.com>
2024-02-13 15:19:24 -0300

    Fixing config errors

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConf = {
+    apiKey: "AIzaSyCvD0sATO41llZ-9F_8d58lYFssFQSL6wY",
+    authDomain: "ioni2-chat.firebaseapp.com",
+    storageBucket: "ioni2-chat.appspot.com",
+    messagingSenderId: "172270112544",
+    appId: "1:172270112544:web:c98f8984688cc7b76c9145",
+    databaseURL: "https://ioni2-chat-default-rtdb.firebaseio.com/"
+  };
